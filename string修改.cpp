1973cba285e45a586d0ca60bb35c61138beff21c
lxkeepcoding <88451719+lxkeepcoding@users.noreply.github.com>
2021-08-26 23:20:15 +0800

    Add files via upload

---
+++ string修改.cpp
@@ -0,0 +1,62 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s1="hello,world";
+//	cout << s1 << endl;
+//	cout << endl;
+//	cout << endl;
+//	s1.push_back('!');
+//	cout << s1 << endl;
+//	return 0;
+//}
+
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s2 = "hello,world";
+//	cout << s2 << endl;
+//	cout << endl;
+//	cout << endl;
+//	s2.append("!!!!");
+//	cout << s2 << endl;
+//	return 0;
+//}
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s3 = "hello,world";
+//	cout << s3 << endl;
+//	cout << endl;
+//	cout << endl;
+//	s3 += '!';
+//	cout << s3 << endl;
+//	cout << endl;
+//	cout << endl;
+//	s3 += "!!!!";
+//	cout << s3 << endl;
+//	cout << endl;
+//	cout << endl;
+//	return 0;
+//}
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s5 = "hello,world";
+//	cout << s5 << endl;
+//	cout << endl;
+//	cout << endl;
+//	s5.erase(2, 3);
+//	cout << s5 << endl;
+//	return 0;
+//}
