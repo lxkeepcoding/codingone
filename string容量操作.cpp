4fb404284e2961661ed479393a6d63b94cd1149d
lxkeepcoding <88451719+lxkeepcoding@users.noreply.github.com>
2021-08-28 02:33:19 +0800

    Add files via upload

---
+++ string容量操作.cpp
@@ -0,0 +1,148 @@
+/ #include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s1="hello,world";
+//	cout << s1 << endl;
+//	cout << endl;
+//	cout << endl;
+//	s1.push_back('!');
+//	cout << s1 << endl;
+//	return 0;
+//}
+
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s2 = "hello,world";
+//	cout << s2 << endl;
+//	cout << endl;
+//	cout << endl;
+//	s2.append("!!!!");
+//	cout << s2 << endl;
+//	return 0;
+//}
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s3 = "hello,world";
+//	cout << s3 << endl;
+//	cout << endl;
+//	cout << endl;
+//	s3 += '!';
+//	cout << s3 << endl;
+//	cout << endl;
+//	cout << endl;
+//	s3 += "!!!!";
+//	cout << s3 << endl;
+//	cout << endl;
+//	cout << endl;
+//	return 0;
+//}
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string s5 = "hello,world";
+//	cout << s5 << endl;
+//	cout << endl;
+//	cout << endl;
+//	s5.erase(2, 3);
+//	cout << s5 << endl;
+//	return 0;
+//}
+
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main()
+//{
+//	string s6="hello,word";
+//	s6.reserve(20);
+//	cout<<s6.capacity()<<endl;
+//	return 0;
+//}
+
+//void Teststring2()
+//{
+//	string s;
+//	// 测试reserve是否会改变string中有效元素个数
+//	s.reserve(100);
+//	cout << s.size() << endl;
+//	cout << s.capacity() << endl;
+//	// 测试reserve参数小于string的底层空间大小时，是否会将空间缩小
+//	s.reserve(50);
+//	cout << s.size() << endl;
+//	cout << s.capacity() << endl;
+//}
+//
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main()
+//{
+//	string s6 = "hello,word";
+//	cout << s6.capacity() << endl;
+//
+//	s6.resize(8);
+//	cout << s6 << endl;
+//	cout << s6.capacity() << endl;
+//
+//	s6.resize(15);
+//	cout << s6.capacity() << endl;
+//	cout << s6 << endl;
+//
+//	s6.resize(20,'m');
+//	cout << s6.capacity() << endl;
+//	cout << s6 << endl;
+//
+//	return 0;
+//}
+
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main()
+//{
+//	string s7 = "hello,word";
+//	cout << s7.capacity() << endl << endl;
+//
+//	s7.reserve(50);
+//	cout << s7.capacity() << endl<<endl;
+//
+//	s7.reserve(100);
+//	cout << s7.capacity() << endl<<endl;
+//	return 0;
+//}
+
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//
+//int main()
+//{
+//	string s7 = "hello,word";
+//	cout << s7.capacity() << endl << endl;
+//
+//	s7.reserve(50);
+//	cout << s7.capacity() << endl << endl;
+//
+//	s7.reserve(100);
+//	cout << s7.capacity() << endl << endl;
+//	return 0;
+//}
